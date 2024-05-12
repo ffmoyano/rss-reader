2ff76e6644688fddfaebfa453689312f9e6d4dc7
ffmoyano <ffmoyano@outlook.com>
2024-05-12 19:50:07 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import feedparser
+
+from flask import Flask, render_template
+import jinja_partials
+
+feed = {
+    'title': 'The Teclado Blog',
+    'href': 'https://blog.teclado.com/rss/',
+    'show_images': True,
+    'entries': {},
+}
+
+
+def create_app():
+    app = Flask(__name__)
+    jinja_partials.register_extensions(app)
+
+    @app.route('/feed/')
+    def render_feed():
+        feed_url = feed['href']
+        parsed_feed = feedparser.parse(feed_url)
+        for entry in parsed_feed.entries:
+            if entry.link not in feed['entries']:
+                feed['entries'][entry.link] = entry
+
+        return render_template('feed.html', feed=feed, entries=feed['entries'].values())
+
+    return app
