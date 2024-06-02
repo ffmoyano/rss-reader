74e83822eb48df655c8748dfd4faf4295dc76eed
ffmoyano <ffmoyano@outlook.com>
2024-06-02 04:52:33 +0200

    course end

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import feedparser
 
-from flask import Flask, render_template, abort, request
+from flask import Flask, render_template, abort, request, redirect, url_for
 import jinja_partials
 
 feeds = {
@@ -31,7 +31,7 @@ def render_feed(feed_url: str = None):
         parsed_feed = feedparser.parse(url)
         for entry in parsed_feed.entries:
             if entry.link not in feed_['entries']:
-                feed_['entries'][entry.link] = entry
+                feed_['entries'][entry.link] = {**entry, "read": False}
 
     if feed_url is None:
         feed = list(feeds.values())[0]
@@ -57,3 +57,20 @@ def render_feed_entries(feed_url: str):
         page=page,
         max_page=len(feed['entries']) // 5,
     )
+
+
+@app.route('/feed/<path:feed_url>/entry/<path:entry_url>')
+def read_entry(feed_url: str, entry_url: str):
+    feed = feeds[feed_url]
+    entry = feed['entries'][entry_url]
+    entry['read'] = True
+    return redirect(entry_url)
+
+
+@app.route('/add_feed', methods=['POST'])
+def add_feed():
+    feed = request.form.get('url')
+    title = request.form.get('title')
+    show_images = request.form.get('showImages') == 'on'
+    feeds[feed] = {"title": title, "href": feed, "show_images": show_images, "entries": {}}
+    return redirect(url_for('render_feed', feed=feed))
